e6fd31d822b7999b4fac8778c18d11b4f9be9358
Oliver Witzki <oliver.witzki@chrono24.com>
2022-09-11 15:53:25 +0200

    Add guest user with tests

---
+++ tests/GuestTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Huppys\BookMe\tests;
+
+use Huppys\BookMe\Guest;
+use PHPUnit\Framework\TestCase;
+
+class GuestTest extends TestCase {
+
+    private string $_firstname;
+    private string $_lastname;
+    private string $_email;
+    private string $_postalCode;
+    private string $_city;
+    private string $_street;
+    private string $_houseNumber;
+
+    private Guest $_guest;
+
+    public function setUp(): void {
+
+        $this->_firstname = "FirstNameTest";
+        $this->_lastname = "LastNameTest";
+        $this->_email = "testuser@example.com";
+        $this->_postalCode = "12345";
+        $this->_city = "Test City";
+        $this->_street = "Test street";
+        $this->_houseNumber = "123";
+
+        $this->_guest = new Guest($this->_firstname, $this->_lastname, $this->_email, $this->_postalCode, $this->_city, $this->_street, $this->_houseNumber);
+    }
+
+    public function test_guestExists(): void {
+        $this->assertInstanceOf(Guest::class, $this->_guest);
+    }
+}
